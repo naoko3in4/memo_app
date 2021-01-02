ea9389a4e4472b90e1d825cde6ea29c6eaf6bb3e
Naoko3in4 <okashimochi@gimail.com>
2021-01-02 11:23:48 +0900

    insert user data into user table

---
+++ backend/database/migrations/2021_01_01_074910_create_memos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            //$table->id();
+            $table->increments('id');
+            $table->string('content', '599');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
